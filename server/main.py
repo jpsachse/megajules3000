9d2eb27364dcb5d2821680d95e814444a3601c72
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-10-24 17:12:37 +0200

    Merge image and collision map generation with server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask
+from flask import Flask, url_for
 from flask.ext.cors import CORS
 
 from mapmanager import MapManager
@@ -8,14 +8,17 @@ from mapmanager import MapManager
 app = Flask(__name__)
 CORS(app)
 
-map_manager = MapManager(map_directory="maps/", initial_map="Paris.json")
+map_manager = MapManager(map_directory="maps/")
+map_manager.change_map(0)
 
 @app.route('/current_map')
 def get_map():
+    map = map_manager.current_map
     response = dict()
-    response["name"] = "Sample"
-    response["objects"] = "{}"
-    response["map"] = "[[{}, {}], [{}, {}]]"
+    response["name"] = map.name
+    map.as_image().save("static/" + map.name + ".png")
+    response["objects"] = url_for('static', filename=map.name+'.png')
+    response["map"] = map.as_collision_map()
     return json.dumps(response)
 
 
--- server/model/tilemap.py
+++ server/model/tilemap.py
@@ -21,7 +21,7 @@ class TileMap():
         for w in range(0, self.width):
             for h in range(0, self.height):
                 current_tile = self.matrix[w][h]
-                current_tile_im = Image.open("mock/"+current_tile.image)
+                current_tile_im = Image.open("maps/" + current_tile.image)
                 box = (w*tile_size, h*tile_size) # upper left corner
                 complete_image.paste(current_tile_im, box)
         return complete_image
