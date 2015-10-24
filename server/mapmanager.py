8c842bc8dd180f51f1b4f56609529d864d51b994
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-10-24 16:17:00 +0200

    Add map manager

---
+++ server/mapmanager.py
@@ -0,0 +1,27 @@
+import os.path
+
+from mock_generator import MockGenerator
+
+
+class MapManager():
+
+    def __init__(self, map_directory="maps/", initial_map=None):
+        self.directory = map_directory
+        self.current_map = initial_map
+        self.maps = self.retrieve_maps()
+
+
+    def retrieve_maps(self):
+        if self.directory == None or not os.path.exists(self.directory):
+            raise Exception("Invalid maps directory")
+
+        generator = MockGenerator()
+        for map_file in os.listdir(self.directory):
+            map_path = self.directory + map_file
+            map = generator.generate_map(map_path)
+            map.name = map_file
+            self.maps.append(map)
+
+
+    def get_map(self, index):
+        return self.maps[index]
