8d83a83a722798652382647007e4ab7a00bd4d6e
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-10-24 15:27:29 +0200

    Add pillow to requirements

--- server/model/tilemap.py
+++ server/model/tilemap.py
@@ -7,3 +7,6 @@ class TileMap():
         self.matrix = list()
         for w in range(0, width):
             self.matrix.append([Tile() for h in range(0, height)])
+
+    def as_image(self):
+        pass
