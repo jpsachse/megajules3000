c747b7fbba0c35408e699b7ec1c5bd86a08a14ca
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2015-10-25 12:24:11 +0100

    Fix next actions in general and an example in Big City

--- server/map_generator.py
+++ server/map_generator.py
@@ -26,25 +26,25 @@ class MapGenerator:
                     tile = map["tiles"][str(tile_type)]
                     tile_action_type = tile.get("action")
                     if tile_action_type:
-                        new_index = current_action_index
-                        new_action = Action(id=new_index, type=tile_action_type, content=tile["content"])
+                        new_action = Action(id=current_action_index, type=tile_action_type, content=tile["content"])
                         result.actions.append(new_action)
                         result.matrix[h][w] = Tile(image=tile["image"]
                                                    , collision=tile["collision"]
-                                                   , action_index=new_index)
+                                                   , action_index=current_action_index)
                         current_action_index += 1
-                        next_action = tile.get("next_action")
+                        next_action_tile = tile.get("next_action")
                         previous_action = tile
                         previous_action_object = new_action
-                        while next_action:
-                            new_index = current_action_index
-                            previous_action_object.next_action = new_index
-                            new_action_object = Action(id=new_index, type=tile_action_type, content=tile["content"])
+                        while next_action_tile:
+                            print "hasNext"
+                            print next_action_tile
+                            previous_action_object.next_action = current_action_index
+                            new_action_object = Action(id=current_action_index, type=next_action_tile["action"], content=next_action_tile["content"])
                             result.actions.append(new_action_object)
                             current_action_index += 1
-                            previous_action = next_action
+                            previous_action = next_action_tile
                             previous_action_object  = new_action_object
-                            next_action = next_action.get("next_action")
+                            next_action_tile = next_action_tile.get("next_action")
                     else:
                         result.matrix[h][w] = Tile(image=tile["image"]
                                                    , collision=tile["collision"]
