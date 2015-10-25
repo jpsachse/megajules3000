4c5c45308b88f464e42d5affe302b4fe4090f68a
Maximilian Grundke <maximiliangrundke@googlemail.com>
2015-10-25 10:05:18 +0100

    Cache facts

---
+++ server/knowledge/fact_cacher.py
@@ -0,0 +1,20 @@
+import json
+
+from fetcher import KnowledgeFetcher
+
+
+class FactCacher():
+
+    def __init__(self):
+        self.fetcher = KnowledgeFetcher()
+
+
+    def cache(self, entity):
+        self.fetcher.get_filtered_facts_for(entity)
+        with open(KnowledgeFetcher.FACT_CACHE_FILE, "w") as f:
+            json.dump(self.fetcher.facts_cache, f)
+
+
+    def cache_all(self, entities):
+        for entity in entities:
+            self.cache(entity)
